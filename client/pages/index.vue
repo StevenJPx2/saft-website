4061e6d88d108f78b344d74f28e8c7bdf8c3bc73
Steven John <stevenjpx@gmail.com>
2020-10-28 19:43:30 +0530

    Using UI library

---
+++ client/pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <h1 class="main-header">
+      Seeking
+      <br />
+      answers, <br />finding <br />truth.
+    </h1>
+    <div id="head-oval" class="blue-oval"></div>
+    <div class="content"><p>Hello</p></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  overflow-x: clip;
+
+  .main-header {
+    @apply uppercase;
+    @apply leading-none;
+    @apply font-black;
+    font-size: Min(15vw, 5rem);
+  }
+
+  #head-oval {
+    @apply relative;
+    left: 50%;
+    top: -10%;
+    width: Min(400px, 60vw);
+    height: Min(400px, 60vw);
+  }
+}
+
+@screen md {
+  .container {
+    overflow-x: initial;
+  }
+}
+</style>
