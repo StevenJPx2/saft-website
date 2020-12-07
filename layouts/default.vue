39619d9cc24ec45a424ad50a2728fb975a25693e
Steven John <stevenjpx@gmail.com>
2020-12-07 18:35:55 +0530

    feat(navbar): Created mobile and desktop version

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,16 @@
 <template>
   <div>
     <nav id="navbar">
-      <img src="@/assets/saft-typeform.svg" alt="SAFT Logo" class="mb-2" />
-      <li class="links">
+      <img
+        id="typeform"
+        src="@/assets/saft-typeform.svg"
+        alt="SAFT Logo"
+        class="mb-2"
+      />
+      <a href="#" id="hamburger" @click="hamburgerClick()">
+        <img src="@/assets/hamburger-icon.svg" alt="Menu" />
+      </a>
+      <li class="links hidden">
         <ul>
           <a href="#">Home</a>
         </ul>
@@ -23,26 +31,49 @@
   </div>
 </template>
 
+<script>
+export default {
+  methods: {
+    hamburgerClick() {
+      const el = document.querySelector(".links");
+      el.classList.toggle("flex");
+      el.classList.toggle("hidden");
+    },
+  },
+};
+</script>
+
 <style lang="scss" scoped>
 #navbar {
   @apply px-6;
   @apply py-4;
   @apply flex;
+  @apply flex-wrap;
   @apply items-center;
+  @apply shadow-lg;
+
   background-color: #001f6d;
 
-  .links {
-    @apply flex;
-    @apply ml-auto;
-    @apply text-white;
-    height: max-content;
+  #typeform {
+    width: 237px;
+  }
 
+  #hamburger {
+    @apply ml-auto;
+  }
+
+  .links {
+    @apply flex-col;
+    @apply text-white;
+    @apply my-3;
+
+    height: max-content;
     width: max-content;
 
     ul {
       @apply mr-5;
       @apply py-2;
-      @apply px-6;
+
       @apply rounded-sm;
       @apply uppercase;
       @apply font-bold;
@@ -51,6 +82,34 @@
       &#patreon-link {
         background-color: #f76754;
         @apply mr-0;
+        @apply mt-2;
+        @apply px-6;
+      }
+    }
+  }
+}
+
+@screen md {
+  #navbar {
+    #typeform {
+      width: 358px;
+    }
+
+    #hamburger {
+      @apply hidden;
+    }
+
+    .links {
+      @apply my-0;
+      @apply flex-row;
+      @apply ml-auto;
+
+      ul {
+        @apply px-6;
+
+        &#patreon-link {
+          @apply mt-0;
+        }
       }
     }
   }
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,28 +1,31 @@
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
-  target: 'static',
+  target: "static",
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'client',
+    title: "client",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      {
+        rel: "stylesheet",
+        href:
+          "https://fonts.googleapis.com/css2?family=Arbutus+Slab&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+      },
+    ],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  '~/assets/css/main.scss'
-  ],
+  css: ["~/assets/css/main.scss"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -30,15 +33,15 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/fontawesome'
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/fontawesome",
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
-    ['nuxt-fontawesome', { component: 'fa' }],
+    "@nuxtjs/axios",
+    ["nuxt-fontawesome", { component: "fa" }],
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -53,6 +56,5 @@ export default {
   },
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {},
+};
