db609ca12b5ed66a2524125e15ff52c83d44431a
Steven John <stevenjpx@gmail.com>
2020-12-26 21:03:14 +0530

    fix(podcast): lesser awkward?

--- pages/podcast/index.vue
+++ pages/podcast/index.vue
@@ -206,7 +206,6 @@ export default {
         trigger: item.name,
         start: "top center",
         end: "top top",
-        markers: true,
         onEnter() {
           animation.restart();
         },
