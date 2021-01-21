27e0a2728670424b05034eef7c076ae8f2cabd04
Steven John <stevenjpx@gmail.com>
2021-01-21 19:22:59 +0530

    build(favicon): Updated favicons

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,8 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+       { name: 'msapplication-TileColor', content: '#da532c' },
+      { name: 'theme-color', content: '#ffffff' },     { hid: "description", name: "description", content: "" },
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
@@ -17,6 +18,19 @@ export default {
         rel: "stylesheet",
         href:
           "https://fonts.googleapis.com/css2?family=Arbutus+Slab&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap",
+      },
+       {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      { rel: 'icon', type: 'image/png', href: '/favicon-32x32.ico' },
+      { rel: 'icon', type: 'image/png', href: '/favicon-16x16.ico' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      {
+        rel: 'mask-icon',
+        href: '/safari-pinned-tab.svg',
+        color: '#000000',
       },
     ],
   },
