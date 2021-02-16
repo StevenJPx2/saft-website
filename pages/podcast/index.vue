e49b34841111de685d38e555da7dc3cddb0b0c61
Steven John <stevenjpx@gmail.com>
2021-02-16 15:33:53 +0530

    feat(podcast): updated podcast number

--- pages/podcast/index.vue
+++ pages/podcast/index.vue
@@ -139,7 +139,7 @@ export default {
 
     gsap.registerPlugin(ScrollTrigger);
 
-    let [countryNo, continentNo] = [44, 6];
+    let [countryNo, continentNo] = [45, 6];
 
     document.querySelectorAll("[aos]").forEach((el) => {
       gsap.fromTo(
